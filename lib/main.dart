cf0db3a901c5a844166cc85267f253241ed89711
Mirko <dlyapisemzzz@yandex.ru>
2021-11-23 00:38:56 +0300

    Навигация

--- lib/main.dart
+++ lib/main.dart
@@ -16,12 +16,20 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(),
+      home: Page(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class Page extends StatefulWidget {
+  const Page({Key? key}) : super(key: key);
+
+  @override
+  _PageState createState() => _PageState();
+}
+
+class _PageState extends State<Page> {
+// class Page extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,30 +41,27 @@ class MyHomePage extends StatelessWidget {
             fontSize: 22,
             fontWeight: FontWeight.bold),
       ),
-      body: Container(
-        width: double.infinity,
-        padding: EdgeInsets.only(top: 22, bottom: 16, left: 16, right: 16),
-        child: Column(
-          mainAxisSize: MainAxisSize.max,
-          children: [
-            BlueContainer(title: '4 квадрата'),
-            BlueContainer(title: 'Цветной список'),
-            BlueContainer(title: 'Codelab'),
-            BlueContainer(title: 'Асинхронный запрос'),
-          ],
-        ),
+      body: _mainScreen(),
+    );
+  }
+
+  Widget _mainScreen() {
+    return Container(
+      width: double.infinity,
+      padding: EdgeInsets.only(top: 22, bottom: 16, left: 16, right: 16),
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          _createNavRow('4 квадрата', FourSquarePage()),
+          _createNavRow('Цветной список', ColorListPage()),
+          _createNavRow('Codelab', CodelabPage()),
+          _createNavRow('Асинхронный запрос', AsyncQueryPage()),
+        ],
       ),
     );
   }
-}
 
-class BlueContainer extends StatelessWidget {
-  final String title;
-
-  BlueContainer({Key? key, this.title = ''}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
+  Widget _createNavRow(String title, Widget page){
     return Container(
       height: 48,
       width: double.infinity,
@@ -84,10 +89,73 @@ class BlueContainer extends StatelessWidget {
               icon: Icon(
                 IconData(57504, fontFamily: 'MaterialIcons'),
               ),
-              onPressed: null,
+              onPressed: _openPage(page),
             ),
           ),
         ],
+      ),
+    );
+  }
+
+  _openPage(Widget page) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => page),
+    );
+  }
+}
+
+class FourSquarePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+      title: const Text('4 квадрата'),
+      ),
+      body: Container(
+        child: Text('4 квадрата')
+      ),
+    );
+  }
+}
+
+class ColorListPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Цветной список'),
+      ),
+      body: Container(
+          child: Text('Цветной список')
+      ),
+    );
+  }
+}
+
+class CodelabPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Кодлаб'),
+      ),
+      body: Container(
+          child: Text('Кодлаб')
+      ),
+    );
+  }
+}
+
+class AsyncQueryPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Асинк'),
+      ),
+      body: Container(
+          child: Text('Асинк')
       ),
     );
   }
